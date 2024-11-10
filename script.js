c5174cd49090d9f2c16b83346dfcbabb155c53fc
Mostafa Kamari <mostafamf555@gmail.com>
2024-11-10 20:06:02 +0330

    add files

---
+++ script.js
@@ -0,0 +1,46 @@
+const userInput = document.querySelector('#date')
+userInput.max = new Date().toISOString().split("T")[0]
+const result = document.querySelector('.result')
+
+function calculate() {
+    let Birthday = new Date(userInput.value)
+
+    let d1 = Birthday.getDay()
+    let m1 = Birthday.getMonth() + 1
+    let y1 = Birthday.getFullYear()
+
+    let today = new Date()
+
+    let d2 = today.getDay()
+    let m2 = today.getMonth() + 1
+    let y2 = today.getFullYear()
+
+    let d3 , m3 , y3;
+
+    y3 = y2 - y1;
+
+    if (m2 >= m1) {
+        m3 = m2 - m1
+    } else {
+        y3--;
+        m3 = 12 + m2 - m1
+    }
+
+    if (d2 >= d1) {
+        d3 = d2 - d1
+    } else {
+        m3--
+        d3 = getDaysInMonth(y1 , m1) + d2 - d1
+    }
+
+    if(m3 < 0) {
+        m3 = 11
+        y3--
+    }
+
+    result.innerHTML = `سن شما ${y3} سال و ${m3} ماه و ${d3} روز است.`
+}
+
+function getDaysInMonth(year , month) {
+    return new Date(year , month , 0).getDate()
+}
